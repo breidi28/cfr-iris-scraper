8384f1d1b0a90a28730c8a6767522db37401b329
V.A. Bradiceanu <vladbradiceanuandrei@gmail.com>
2026-02-22 17:19:02 +0200

    removed tests

--- check_tip_oprire.py
+++
@@ -1,5 +0,0 @@
-import re
-with open('trenuri-2025-2026_sntfc.xml', 'r', encoding='utf-8') as f:
-    text = f.read()
-    matches = set(re.findall(r'TipOprire="([^"]*)"', text))
-    print(matches)
